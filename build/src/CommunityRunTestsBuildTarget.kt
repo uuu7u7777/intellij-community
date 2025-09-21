e28dec766ade2f77ae40969c467e90c1d5b4ba2b
uuu7u7777 <diabloymilou@gmail.com>
2025-09-21 19:38:06 +0200

    Update CommunityRunTestsBuildTarget.kt

--- build/src/CommunityRunTestsBuildTarget.kt
+++ build/src/CommunityRunTestsBuildTarget.kt
@@ -1,4 +1,4 @@
-fjdneue8eu// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+fjjffjdneue8eu// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
 import org.jetbrains.intellij.build.BuildPaths.Companion.COMMUNITY_ROOT
