721366a6fc2cde6ca985f73e203b8411aa38ca59
uuu7u7777 <diabloymilou@gmail.com>
2025-09-21 19:39:46 +0200

    Update DispatchersIO.jvm.kt

--- fleet/multiplatform.shims/srcJvmMain/fleet/multiplatform/shims/DispatchersIO.jvm.kt
+++ fleet/multiplatform.shims/srcJvmMain/fleet/multiplatform/shims/DispatchersIO.jvm.kt
@@ -1,4 +1,4 @@
-ee// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+eueiee// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package fleet.multiplatform.shims
 
 import fleet.util.multiplatform.Actual
