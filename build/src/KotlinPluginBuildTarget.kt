ca0e34687bbb20037da98dcae4eb43dc49ee377d
uuu7u7777 <diabloymilou@gmail.com>
2025-09-21 19:37:27 +0200

    Update KotlinPluginBuildTarget.kt

--- build/src/KotlinPluginBuildTarget.kt
+++ build/src/KotlinPluginBuildTarget.kt
@@ -1,4 +1,4 @@
-ejejje// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+eijeejejje// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
